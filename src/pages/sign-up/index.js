993a1c1fb62a55311b1c02a36c56e0b4165c19a3
bluehouse <bluehouse@bluehouses-MacBook-Pro.local>
2021-06-05 21:36:01 +0100

    accomodate capital letters in email

--- src/pages/sign-up/index.js
+++ src/pages/sign-up/index.js
@@ -112,7 +112,8 @@ const SignUp = (props) => {
             name="email"
             id="email"
             value={email}
-            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,9}$"
+            // pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+            pattern="[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-zA-Z]{2,9}$"
             type="email"
             label="Email"
           />
