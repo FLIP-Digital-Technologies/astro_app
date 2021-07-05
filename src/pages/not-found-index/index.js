609872e025734ca019ac81b6b75b892622b2f527
bluehouse <bluehouse@bluehouses-MacBook-Pro.local>
2021-07-05 11:19:51 +0100

    maintenance display 1

--- src/pages/not-found-index/index.js
+++ src/pages/not-found-index/index.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { AuthHeader } from "../../components/header";
+import png from "../../assets/png";
+import { AnotherHeader } from "../../components/header";
 import styles from "../styles.module.scss";
 
 export default function NotFound() {
   return (
-    <AuthHeader>
+    <AnotherHeader>
       <div className={styles.notfoundindex}>
-        <h1>404</h1>
-        <h2>Oops, Page Not Found</h2>
+      <img
+        src={png.UnderMaintenance}
+        
+        />
+        <h1>Astro will be back shortly</h1>
+        <h2>We are under Maintenance</h2>
+        <h3>Please bear with us</h3>
       </div>
-    </AuthHeader>
+    </AnotherHeader>
   );
 }
